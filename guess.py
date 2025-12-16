7eafd9dedb751e80b9638cd4e6acb0942f00e337
Satish Surjoo <surjoo@icloud.com>
2025-12-16 08:05:12 +0200

    new files added

--- guess.py
+++ guess.py
@@ -1,18 +1,32 @@
+# This is a Guess the Number game.
 import random
 
-NUM_DIGITS =3
-MAX_GUESSES = 10
+guessesTaken = 0
 
-def main():
-    print('''Bagels, a deductive logic Game
-            By Al Sweigart
-            I am thinking of a {}-digit number with no repeated digits.
-          Try tp guess what it is. Here are some clues:
-          When I say:    That means:
-          Pico         One digit is correct but in the wrong position.
-            Fermi        One digit is correct and in the right position.
-            Bagels       No digit is correct.
-            For example, if the secret number was 248 and your guess was 843, the
-            clues would be Fermi Pico.'''.format(NUM_DIGITS))
-    
-    
+print('Hello! What is your name?')
+myName = input()
+
+number = random.randint(1, 20)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+
+for guessesTaken in range(6):
+    print('Take a guess.') # Four spaces in front of "print".
+    guess = input()
+    guess = int(guess)
+
+    if guess < number:
+        print('Your guess is too low.') # Eight spaces in front of "print"
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number + '.')
